d6d5086d24235d84e6f7398bfcb2ab8113b62cdb
Unknown <kiddid96@gmail.com>
2017-11-13 18:24:28 +0100

    Fix operationalize

--- Motob.py
+++ Motob.py
@@ -1,26 +1,35 @@
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath("motors"))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath("zumo_button"))))
+
+from motors import Motors
+from zumo_button import ZumoButton
+
+
 class Motob:
+	def __init__(self, motors):
 
-		def __init__(self, motors):
+		self.motors = motors
+		self.value = None
 
-				self.motors = motors
-				self.value = None
+	def update(self, recommendation):
+		self.operationalize(recommendation)
 
-		def update(self,recommendation):
-				self.operationalize(recommendation)
+	def operationalize(self, recommendation):
+		# todo: Sette motorsfilene slik at self.persist ikke stopper automatisk etter duration, hvis ikke stopper robotten etter hver command
+		if recommendation == 'Turn_Color':
+			self.motors.stop()
+			angle = None  # Sett inn value utfra hvilke sensorer som detekterer linjen
+			self.motor.set_value(angle)
+		if recommendation == 'Turn_Line':
+			self.motors.stop()
+			angle = None  # Sett inn value utfra hvilke sensorer som detekterer linjen
+			self.motor.set_value(angle)
+		if recommendation == 'Standard':
+			self.motors.stop()
+			angle = None  # Sett inn value utfra hvilke sensorer som detekterer linjen
+			self.motor.set_value(angle)
 
-    def operationalize(self,recommendation):
 
-        #todo: Sette motorsfilene slik at self.persist ikke stopper automatisk etter duration, hvis ikke stopper robotten etter hver command
-        if recommendation == 'Turn_Color':
-            for motor in self.motors:
-                motor.stop()
-                angle = None # Sett inn value utifra fargekonsentrasjon i bilde eller noe
-                motor.set_value(angle)
-        if recommendation == 'Turn_Line':
-            for motor in self.motors:
-                motor.stop()
-                angle = None # Sett inn value utfra hvilke sensorer som detekterer linjen
-                motor.set_value(angle)
-        if recommendation == 'Standard':
-            for motor in self.motors:
-                motor.forward()
+ZumoButton().wait_for_press()
+motor = Motob(Motors)
+motor.operationalize("Turn_Color")
