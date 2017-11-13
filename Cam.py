717cfa85fba7613ea65331215038a734e487ec03
Unknown <mortensmeb@gmail.com>
2017-11-13 16:50:23 +0100

    Add image capture to Cam

--- Cam.py
+++ Cam.py
@@ -1,3 +1,22 @@
+from Sensob import Sensob
+import os
+from PIL import Image
+
+
 class Cam(Sensob):
-    def __init__(self, sensors):
-        super().__init__(sensors)
+    def __init__(self, img_width=128, img_height=96, img_rot=0):
+                super().__init__("Camera")
+                self.img_width=img_width
+                self.img_height=img_height
+                self.img_rot=img_rot
+
+    def get_image(self):
+        os.system('raspistill -t 1 -o image.png -w "' + str(self.img_width) + '" -h "' + str(self.img_height) + '" -rot "' + str(self.img_rot) + '"')
+        return Image.open('image.png').convert('RGB')
+
+
+if __name__ == "__main__":
+        cam = Cam()
+        image = cam.get_image()
+        cam.update(image)
+        cam.get_value().save("image123.png")
