840a6174b448c13419ccb11777e2e382bab96e4d
henKnud <33206396+FibonacciHK@users.noreply.github.com>
2017-11-13 18:00:42 +0100

    Update Motob.py

--- Motob.py
+++ Motob.py
@@ -1,16 +1,20 @@
 class Motob:
 
-		def __init__(self, motors):
+    def __init__(self, motors):
 
-				self.motors = motors
-				self.value = None
+        self.motors = motors
+        self.value = None
 
-		def update(self,recommendation):
-				self.operationalize(recommendation)
+    def update(self,recommendation):
+        self.operationalize(recommendation)
 
     def operationalize(self,recommendation):
 
         #todo: Sette motorsfilene slik at self.persist ikke stopper automatisk etter duration, hvis ikke stopper robotten etter hver command
+        if recommendation == 'Halt':
+            for motor in self.motors:
+                motor.stop()
+                time.sleep(100)
         if recommendation == 'Turn_Color':
             for motor in self.motors:
                 motor.stop()
